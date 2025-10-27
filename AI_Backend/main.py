501ca3649311ed9ec5811d34955ced437fcba327
Vikas Gautam <vikasgautamug2003@gmail.com>
2025-10-27 06:47:28 +0530

    fixed cors

--- AI_Backend/main.py
+++ AI_Backend/main.py
@@ -331,7 +331,7 @@ def create_rag_chain(video_id: str):
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://youtuber-video-rag-project.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
